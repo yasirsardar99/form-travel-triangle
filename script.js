85c0eea70f130c778247497f40d7adda2794d1c8
Muhammad Yasir <64388216+yasirsardar99@users.noreply.github.com>
2023-05-11 18:21:58 +0500

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var Form1 = document.getElementById("form1")
 var Form2 = document.getElementById("form2")
 var Form3 = document.getElementById("form3")
+var Form4 = document.getElementById("form4")
+
 
 
 var Next1 = document.getElementById("next1")
@@ -8,6 +10,9 @@ var Next2 = document.getElementById("next2")
 var Next3 = document.getElementById("next3")
 var Back1 = document.getElementById("back1")
 var Back2 = document.getElementById("back2")
+var Back3 = document.getElementById("back3")
+var No = document.getElementById("no")
+
 
 Next1.onclick = function(){
     Form1.style.left="-600px";
@@ -22,11 +27,22 @@ Next2.onclick = function(){
 Back2.onclick = function(){
     Form3.style.left="600px";
     Form2.style.left="5px";
+    Form4.style.left="600px";
 }
 
 Back1.onclick = function(){
     Form2.style.left="-600px";
     Form1.style.left="5px";
+}
+
+Next3.onclick = function(){
+    Form3.style.left="600px";
+    Form4.style.left="-600px";
+}
+
+Back3.onclick = function(){
+    Form3.style.left="5px";
+    Form4.style.left="-600px";
 }
 
 
